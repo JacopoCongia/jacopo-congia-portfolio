772130a626973c235680588685c55d367f2885c9
jacopo.congia <jacopo.congia@gmail.com>
2023-09-25 11:06:47 +0200

    CSS Fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,35 +6,37 @@ function AboutMe({ aboutRef, aboutInView }) {
       <section
         ref={aboutRef}
         id="about"
-        className={`min-h-[100vh] flex flex-col items-center justify-center mx-auto gap-[10%] pt-[100px] pb-[70px] px-[3em] text-[1.1rem] text-center text-neutral-200 bg-[#200333] min-[1200px]:flex-row min-[1200px]:items-start min-[1200px]:px-[10em] min-[1200px]:pt-[150px] duration-[0.3s] opacity-0 ${
+        className={`min-h-[100vh] flex flex-col items-center justify-center mx-auto gap-[10%] pt-[100px] pb-[70px] px-[3em] text-[1.1rem] text-center text-neutral-200 bg-[#200333]  duration-[0.3s] opacity-0 ${
           aboutInView && "opacity-100"
         }`}
       >
-        <div
-          className={`flex flex-col items-center gap-[4em] min-[1200px]:max-w-[50%]`}
-        >
-          <h1 className="text-[1.9rem] font-bold border-l-[0.2em] pl-[0.5em] border-l-orange-500 min-[800px]:text-[2.5rem] min-[1200px]:self-start">
-            About Me
-          </h1>
-          <img
-            src="/avatar.jpeg"
-            className="w-[180px] border-[5px] border-orange-500 rounded-[50%] mb-[-25px]"
-          />
-          <p className="mb-[3em] leading-[1.6em] min-[800px]:max-w-[66%] min-[1200px]:max-w-[100%] min-[1400px]:max-w-[100%]">
-            With a background in{" "}
-            <span className="font-semibold">Photography</span> and{" "}
-            <span className="font-semibold">Photo Editing</span>, I started my
-            coding journey by making simple websites, then I moved on to{" "}
-            <span className="font-semibold">JavaScript</span> and{" "}
-            <span className="font-semibold">React.js</span>, and it opened my
-            eyes to a whole new world. I love learning about new things, and the
-            possibilities in this field are endless. Outside of coding, I enjoy
-            powerlifting, playing video games, taking pictures and another
-            never-ending learning endeavor: cooking.
-          </p>
-        </div>
+        <div className="flex flex-col min-[1200px]:flex-row min-[1200px]:items-start min-[1200px]:px-[10em]">
+          <div
+            className={`flex flex-col items-center gap-[4em] min-[1200px]:max-w-[50%]`}
+          >
+            <h1 className="text-[1.9rem] font-bold border-l-[0.2em] pl-[0.5em] border-l-orange-500 min-[800px]:text-[2.5rem] min-[1200px]:self-start">
+              About Me
+            </h1>
+            <img
+              src="/avatar.jpeg"
+              className="w-[180px] border-[5px] border-orange-500 rounded-[50%] mb-[-25px]"
+            />
+            <p className="mb-[3em] leading-[1.6em] min-[800px]:max-w-[66%] min-[1200px]:max-w-[100%] min-[1400px]:max-w-[100%]">
+              With a background in{" "}
+              <span className="font-semibold">Photography</span> and{" "}
+              <span className="font-semibold">Photo Editing</span>, I started my
+              coding journey by making simple websites, then I moved on to{" "}
+              <span className="font-semibold">JavaScript</span> and{" "}
+              <span className="font-semibold">React.js</span>, and it opened my
+              eyes to a whole new world. I love learning about new things, and
+              the possibilities in this field are endless. Outside of coding, I
+              enjoy powerlifting, playing video games, taking pictures and
+              another never-ending learning endeavor: cooking.
+            </p>
+          </div>
 
-        <Skills />
+          <Skills />
+        </div>
       </section>
     </div>
   );
