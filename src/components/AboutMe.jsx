68597ba7b463399f3d7c94cb4175630c0c49c715
jacopo.congia <jacopo.congia@gmail.com>
2025-11-17 14:36:45 +0100

    Updated Color Pallette

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,36 +2,42 @@ import Skills from "./Skills";
 
 function AboutMe({ aboutRef, aboutInView }) {
   return (
-    <div className="min-h-[100vh] bg-[#200333]">
+    <div className="min-h-[100vh] bg-retro-brown">
       <section
         ref={aboutRef}
         id="about"
-        className={`min-h-[100vh] flex flex-col items-center justify-center mx-auto gap-[10%] pt-[100px] pb-[70px] px-[3em] text-[1.1rem] text-center text-neutral-200 bg-[#200333]  duration-[0.3s] opacity-0 ${
+        className={`min-h-[100vh] flex flex-col items-center justify-center mx-auto gap-[10%] pt-[100px] pb-[70px] px-[3em] text-[1.1rem] text-center text-retro-cream bg-retro-brown duration-[0.3s] opacity-0 ${
           aboutInView && "opacity-100"
         }`}
       >
-        <div className="flex flex-col min-[1200px]:flex-row min-[1200px]:items-start min-[1200px]:px-[10em] min-[1200px]:gap-[3em] min-[1200px]:justify-center">
+        <div className="flex flex-col min-[1200px]:flex-row min-[1200px]:items-start min-[1200px]:px-[10em] min-[1200px]:gap-[5em] min-[1200px]:justify-center">
           <div
             className={`flex flex-col items-center gap-[4em] min-[1400px]:max-w-[35%]`}
           >
-            <h1 className="text-[1.9rem] font-bold border-l-[0.2em] pl-[0.5em] border-l-orange-500 min-[800px]:text-[2.5rem] min-[1200px]:self-start">
+            <h1 className="text-[2.4rem] whitespace-nowrap font-shrikhand border-l-[0.2em] pl-[0.5em] border-l-retro-yellow min-[800px]:text-[3.2rem] min-[1200px]:self-start">
               About Me
             </h1>
             <img
               src="/profile_picture_small.png"
-              className="w-[180px] border-[5px] border-orange-500 rounded-[50%] mb-[-25px]"
+              className="w-[180px] border-[5px] border-retro-yellow rounded-[50%] mb-[-25px]"
             />
             <p className="mb-[3em] leading-[1.9em] min-[800px]:max-w-[66%] min-[1200px]:max-w-[100%] min-[1400px]:max-w-[100%]">
               With a background in{" "}
-              <span className="font-semibold">Photography</span> and{" "}
-              <span className="font-semibold">Photo Editing</span>, I started my
-              coding journey by making simple websites, then I moved on to{" "}
-              <span className="font-semibold">JavaScript</span> and{" "}
-              <span className="font-semibold">React.js</span>, and it opened my
-              eyes to a whole new world. I love learning about new things, and
-              the possibilities in this field are endless. Outside of coding, I
-              enjoy inline skating, playing video games, taking pictures and
-              another never-ending learning endeavor: cooking.
+              <span className="font-semibold text-retro-mint">Photography</span>{" "}
+              and{" "}
+              <span className="font-semibold text-retro-mint">
+                Photo Editing
+              </span>
+              , I started my coding journey by making simple websites, then I
+              moved on to{" "}
+              <span className="font-semibold text-retro-pink">JavaScript</span>{" "}
+              and{" "}
+              <span className="font-semibold text-retro-pink">React.js</span>,
+              and it opened my eyes to a whole new world. I love learning about
+              new things, and the possibilities in this field are endless.
+              Outside of coding, I enjoy inline skating, playing video games,
+              taking pictures and another never-ending learning endeavor:
+              cooking.
             </p>
           </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,27 @@
 function Navbar({ homeInView, aboutInView, projectsInView }) {
   return (
-    <nav className="sticky h-[60px] select-none justify-center flex min-[700px]:justify-end z-50 top-0 shadow-md backdrop-blur-sm bg-orange-500/[85%] px-[1em] font-medium text-[0.9rem] min-[700px]:pr-[2em] min-[700px]:text-[1rem]">
-      <ul className="flex text-neutral-200">
+    <nav className="sticky h-[60px] select-none justify-center flex min-[700px]:justify-end z-50 top-0 shadow-md backdrop-blur-sm bg-retro-yellow/[95%] px-[1em] font-medium text-[0.9rem] min-[700px]:pr-[2em] min-[700px]:text-[1rem]">
+      <ul className="flex text-retro-cream">
         <li
-          className={`h-[100%] hover:border-b-[8px] hover:border-b-orange-600 px-[1em] flex items-center hover:bg-neutral-200 hover:text-orange-500 hover:pt-[8px] ${
+          className={`h-[100%] hover:border-b-[8px] hover:border-b-retro-pink px-[1em] flex items-center hover:bg-retro-cream hover:text-retro-brown hover:pt-[8px] ${
             homeInView &&
-            "bg-neutral-200 text-orange-500 border-b-[8px] pt-[8px] border-b-orange-600"
+            "bg-retro-cream text-retro-brown border-b-[8px] pt-[8px] border-b-retro-pink"
           }`}
         >
           <a href="#home">Home</a>
         </li>
         <li
-          className={`h-[100%] hover:border-b-[8px] hover:border-b-orange-600 px-[1em] flex items-center hover:bg-neutral-200 hover:text-orange-500 hover:pt-[8px] ${
+          className={`h-[100%] hover:border-b-[8px] hover:border-b-retro-pink px-[1em] flex items-center hover:bg-retro-cream hover:text-retro-brown hover:pt-[8px] ${
             aboutInView &&
-            "bg-neutral-200 text-orange-500 border-b-[8px] pt-[8px] border-b-orange-600"
+            "bg-retro-cream text-retro-brown border-b-[8px] pt-[8px] border-b-retro-pink"
           }`}
         >
           <a href="#about">About</a>
         </li>
         <li
-          className={`h-[100%] hover:border-b-[8px] hover:border-b-orange-600 px-[1em] flex items-center hover:bg-neutral-200 hover:text-orange-500 hover:pt-[8px] ${
+          className={`h-[100%] hover:border-b-[8px] hover:border-b-retro-pink px-[1em] flex items-center hover:bg-retro-cream hover:text-retro-brown hover:pt-[8px] ${
             projectsInView &&
-            "bg-neutral-200 text-orange-500 border-b-[8px] pt-[8px] border-b-orange-600"
+            "bg-retro-cream text-retro-brown border-b-[8px] pt-[8px] border-b-retro-pink"
           }`}
         >
           <a href="#myprojects">My Projects</a>
